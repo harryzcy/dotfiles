077fac756ea32344ff383c7567ac650e796ee82c
Chongyi Zheng <harry@harryzheng.com>
2022-04-14 01:14:51 -0400

    Add function for updating go

--- darwin/.functions.zsh
+++ darwin/.functions.zsh
@@ -12,3 +12,38 @@ function set_proxy() {
 function unset_proxy() {
     export https_proxy= http_proxy= all_proxy=
 }
+
+update_go() {
+    version=$1
+    if [ -z "$version" ]; then
+        echo "Usage: update_go <version>"
+        return 1
+    fi
+
+    sudo -v
+
+    echo "Updating go to version $version"
+
+    archstr=$(uname -m)
+    if [[ "$archstr" == "x86_64" ]]; then
+        arch="amd64"
+    elif [[ "$archstr" == "arm64" || "$archstr" == "arm" ]]; then
+        arch="arm64"
+    else
+        echo "Unsupported architecture: $archstr"
+        return 1
+    fi
+
+    url="https://go.dev/dl/go${version}.darwin-${arch}.tar.gz"
+    echo "Downloading $url"
+
+    file="$HOME/Downloads/go${version}.darwin-${arch}.tar.gz"
+    curl -L "$url" -o "$file"
+
+    echo "Extracting $file"
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf "$file"
+    rm "$file"
+
+    echo "Done"
+}
