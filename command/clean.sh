730ce6b85a17fbc0b2b1f2e5c3348193fa8ef13f
Chongyi Zheng <harry@harryzheng.com>
2022-07-15 16:21:54 -0400

    Use sudo permission

--- command/clean.sh
+++ command/clean.sh
@@ -5,9 +5,9 @@ clean:mac() {
 }
 
 clean:debian() {
-  apt-get autoremove -y
-  apt-get autoclean -y
-  apt-get clean -y
+  sudo apt-get autoremove -y
+  sudo apt-get autoclean -y
+  sudo apt-get clean -y
 }
 
 unamestr=$(uname)
