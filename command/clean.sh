e29bddda0edf58901d1a7d9bf364c0b0ede4c478
Chongyi Zheng <harry@harryzheng.com>
2022-11-16 22:06:18 +0000

    [darwin] Fix pip clean issue when there's no cache

--- command/clean.sh
+++ command/clean.sh
@@ -2,7 +2,13 @@
 
 clean:mac() {
   brew cleanup -s
-  pip3 cache remove *
+
+  # clean pip cache
+  local_wheel_str=$(pip3 cache info | grep "locally built wheels")
+  num_wheels=${local_wheel_str#*:} # remove everything before :
+  if (( num_wheels > 0 )); then
+    pip3 cache remove *
+  fi
 }
 
 clean:debian() {
