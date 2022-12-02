1e9d520acb2d3151390ca6bb0b60fb22d05ac735
Chongyi Zheng <harry@harryzheng.com>
2022-12-02 21:48:24 +0000

    Add `dot repo` and `dot goto` commands

---
+++ script/repo.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+repo_name=$1
+
+(
+    if [ $ZSH_VERSION ]; then
+      setopt sh_word_split
+    fi
+
+    IFS=:
+    for p in $DOT_REPO_PATH; do
+        full_path="${p}/${repo_name}"
+        if [ -d "${full_path}" ]; then
+            echo "${full_path}"
+            return
+        fi
+    done
+)
