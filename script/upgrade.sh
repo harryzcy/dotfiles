811df63b05383040a5398df113600230fa5088f5
Chongyi Zheng <harry@harryzheng.com>
2022-12-02 16:58:41 -0500

    Upgrade nvm to v0.39.2 (#18)

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -18,5 +18,5 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
   brew upgrade --cask
 
   # upgrade nvm
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 fi
