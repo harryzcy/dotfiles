2495e5290c906a8fd7da992a3d34b287cfaf4665
Chongyi Zheng <harry@harryzheng.com>
2022-12-04 16:04:44 -0500

    Fix issue `realpath` not installed on macOS (#24)

--- script/setup/setup_darwin.sh
+++ script/setup/setup_darwin.sh
@@ -50,6 +50,7 @@ install_git() {
 
 install_tools() {
   echo "installing tools for macOS"
+  run_brew_install coreutils realpath
   run_brew_install cloc
   run_brew_install curl
   run_brew_install gh
