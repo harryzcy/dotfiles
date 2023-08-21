f0d2aa269b05dc042c9ed0999bde7f2bb8e94a35
Chongyi Zheng <git@zcy.dev>
2023-08-21 01:48:21 -0400

    [darwin] Switch input language on window focus (#75)

--- darwin/hammerspoon/init.lua
+++ darwin/hammerspoon/init.lua
@@ -30,3 +30,25 @@ hs.hotkey.bind({"ctrl", "alt", "cmd"}, "1", moveWindowToDisplay(1))
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "2", moveWindowToDisplay(2))
 
 hs.alert.show("Config loaded")
+
+local function Chinese()
+  hs.console.printStyledtext("swithcing to Chinese")
+  hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
+end
+
+local function English()
+  hs.console.printStyledtext("swithcing to English")
+  hs.keycodes.currentSourceID("com.apple.keylayout.US")
+end
+
+local function set_app_input_method(app_name, set_input_method_function, event)
+  event = event or hs.window.filter.windowFocused
+  hs.window.filter.new(app_name)
+    :subscribe(event, function() set_input_method_function() end)
+end
+
+set_app_input_method('Hammerspoon', English, hs.window.filter.windowCreated)
+set_app_input_method('Spotlight', English, hs.window.filter.windowCreated)
+set_app_input_method('iTerm2', English)
+set_app_input_method('WeChat', Chinese)
+set_app_input_method('Code', English)
