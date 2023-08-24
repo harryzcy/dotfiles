8e615d7f73afd126cd1412c479d61e04a9f2a011
Chongyi Zheng <git@zcy.dev>
2023-08-24 18:52:55 -0400

    Support skipping installing (#78)

--- script/setup.sh
+++ script/setup.sh
@@ -15,20 +15,28 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
   platform='darwin'
 fi
 
+# detect architecture
+arch=$(uname -m)
+if [[ "$arch" == 'aar64' ]]; then
+  arch='arm64'
+fi
+
 source ./setup/setup_common.sh
 
-if [[ ${CODESPACES} == 'true' ]]; then
-  src_dir=${DOTFILE_DIR}/codespace
+if [[ "${CODESPACES}" == 'true' ]]; then
+  src_dir="${DOTFILE_DIR}/codespace"
   source ./setup/setup_linux.sh
 elif [[ ${platform} == 'linux' ]]; then
   # platform specific src directory
-  src_dir=${DOTFILE_DIR}/rpi
+  src_dir="${DOTFILE_DIR}/rpi"
 
   source ./setup/setup_linux.sh
-  run_apt_update
+  if [[ "${NO_INSTALL}" != "true" ]]; then
+    run_apt_update
+  fi
 elif [[ ${platform} == 'darwin' ]]; then
   # platform specific src directory
-  src_dir=${DOTFILE_DIR}/darwin
+  src_dir="${DOTFILE_DIR}/darwin"
 
   source ./setup/setup_darwin.sh
   install_xcode_select
@@ -38,13 +46,15 @@ else
   exit 1
 fi
 
-install_git
+[[ "${NO_INSTALL}" != "true" ]] && install_git
 configure_git ${src_dir}
-install_zsh ${src_dir}
-
+[[ "${NO_INSTALL}" != "true" ]] && install_zsh ${src_dir}
 configure_zsh ${src_dir}
-install_tools
 
-if typeset -f install_software >/dev/null; then
-  install_software
+if [[ "${NO_INSTALL}" != "true" ]]; then
+  install_tools
+
+  if typeset -f install_software >/dev/null; then
+    install_software
+  fi
 fi
