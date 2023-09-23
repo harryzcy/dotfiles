1df746829ca54b9bf194f5073023304fc2aa8f34
Chongyi Zheng <git@zcy.dev>
2023-09-23 17:37:19 -0400

    Fix `npx` and `yarn` auto loading

--- darwin/.functions.zsh
+++ darwin/.functions.zsh
@@ -34,7 +34,7 @@ function chk3s() {
 
 # nvm
 lazy_load_nvm() {
-  unset -f npm node nvm
+  unset -f npm node nvm npx yarn
   export NVM_DIR=~/.nvm
   [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
