2d987bf588db3f3b76795605643b77c11adad81c
Chongyi Zheng <git@zcy.dev>
2023-10-02 09:46:53 -0400

    [darwin] Add some gh aliases

--- darwin/.aliases.zsh
+++ darwin/.aliases.zsh
@@ -1,3 +1,11 @@
 # ansible
 alias a='ansible'
 alias ap='ansible-playbook'
+
+# gh
+alias ghp='gh pr'
+alias ghpc='gh pr create'
+alias ghpw='gh pr view --web'
+alias ghpco='gh pr checkout'
+alias ghps='gh pr status'
+alias ghr='gh repo'
