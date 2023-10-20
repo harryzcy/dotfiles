2f0fbeb5eb55c3d75484e3c28f5cb53fdb705d75
Chongyi Zheng <git@zcy.dev>
2023-10-20 02:26:14 -0400

    Fix omz custom path

--- script/setup/util_common.sh
+++ script/setup/util_common.sh
@@ -36,12 +36,12 @@ configure_zsh() {
   # install plugins
   if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
     echo "installing zsh-autosuggestions"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   fi
 
   if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
     echo "installing zsh-syntax-highlighting"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   fi
 }
 
