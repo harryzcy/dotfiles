c6ef8a72ffacc4ebec24a3d5ff83e41b30193aff
Chongyi Zheng <git@zcy.dev>
2023-10-23 19:50:11 -0400

    Activate conda after setting path

--- linux/dev.environments.zsh
+++ linux/dev.environments.zsh
@@ -1,10 +1,10 @@
-eval "$(/home/harryzcy/miniconda3/bin/conda shell.zsh hook)"
-
 path=(
 	/usr/local/go/bin # Go
 	$path
 	$GOPATH/bin # Go
 )
 export PATH
+
+eval "$(/home/harryzcy/miniconda3/bin/conda shell.zsh hook)"
 
 export DOT_REPO_PATH="${HOME}/Projects:${HOME}/Projects/telepresence"
