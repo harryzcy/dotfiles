887453e02db2a8dac56b3cb9b9b85bb7125a2dc1
Chongyi Zheng <git@zcy.dev>
2023-11-04 06:04:56 -0400

    [linux] Upgrade awscli only if it's installed (#128)

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -21,9 +21,12 @@ if [[ "$os" == 'linux' ]]; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get -yq upgrade
 
   # if awscli is installed, upgrade it
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
-  unzip -o "$HOME/awscliv2.zip" -d "$HOME"
-  sudo "$HOME/aws/install" --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+  if command -v aws &>/dev/null; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
+    unzip -o "$HOME/awscliv2.zip" -d "$HOME"
+    sudo "$HOME/aws/install" --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+    rm -rf "$HOME/awscliv2.zip" "$HOME/aws"
+  fi
 
   if type "pipx" >/dev/null; then
     echo "upgrading pipx packages"
