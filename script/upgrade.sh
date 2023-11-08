ef7d3fe1f64621af82ca446e9d86eec1ba8aef48
Chongyi Zheng <git@zcy.dev>
2023-11-08 03:31:03 -0500

    [dot] Refactor macos upgrade script

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -23,15 +23,5 @@ if [[ "$os" == 'linux' ]]; then
   $DOTFILE_DIR/upgrade/upgrade_linux.sh
 elif [[ "$os" == 'macos' ]]; then
   # macos
-  brew update && brew upgrade
-  brew upgrade --cask
-
-  # krew
-  kubectl krew update
-  kubectl krew upgrade
-
-  # upgrade nvm
-  nvm_latest=$(curl -q -w "%{url_effective}\\n" -L -s -S https://latest.nvm.sh -o /dev/null)
-  nvm_latest=${nvm_latest##*/}
-  curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_latest}/install.sh" | bash
+  $DOTFILE_DIR/upgrade/upgrade_macos.sh
 fi
