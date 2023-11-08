f078c5d6adb572758d85995187cae4e35bf799b4
Chongyi Zheng <git@zcy.dev>
2023-11-08 03:29:55 -0500

    [dev] Move some linux upgrade command

--- script/upgrade/upgrade_linux.sh
+++ script/upgrade/upgrade_linux.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env zsh
 
+# if awscli is installed, upgrade it
+if command -v aws &>/dev/null; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
+  unzip -o "$HOME/awscliv2.zip" -d "$HOME"
+  sudo "$HOME/aws/install" --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+  rm -rf "$HOME/awscliv2.zip" "$HOME/aws"
+fi
+
+if type "pipx" >/dev/null; then
+  echo "upgrading pipx packages"
+  pipx upgrade-all
+fi
+
+if command -v golangci-lint &>/dev/null; then
+  echo "upgrading golangci-lint"
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+fi
+
 if [[ $(hostname -s) = gpu-* ]]; then
   kubectl krew update
   kubectl krew upgrade
