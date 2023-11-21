e2908502b98fe2ce110ae052c08abf571f29da2d
Chongyi Zheng <git@zcy.dev>
2023-11-21 17:37:08 -0500

    [dot] Move upgrade_zsh

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -2,21 +2,11 @@
 
 set -o pipefail
 
-# update oh-my-zsh
-if [ -z "$ZSH" ]; then
-  ZSH="$HOME/.oh-my-zsh"
-fi
-ZSH="$ZSH" command zsh -f "$ZSH/tools/upgrade.sh" || return $?
-
-echo "updating zsh-autosuggestions"
-git -C $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull >/dev/null
-
-echo "updating zsh-syntax-highlighting"
-git -C $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull >/dev/null
-
 # common tools
 source $DOTFILE_DIR/shared/.functions.sh
 source $DOTFILE_DIR/script/upgrade/upgrade_common.sh
+
+upgrade_zsh
 
 os=$(detect_os)
 if [[ "$os" == 'linux' ]]; then
--- script/upgrade/upgrade_common.sh
+++ script/upgrade/upgrade_common.sh
@@ -7,6 +7,21 @@ node_packaegs=(
   yarn
 )
 
+upgrade_zsh() {
+  # upgrade oh-my-zsh
+  if [ -z "$ZSH" ]; then
+    ZSH="$HOME/.oh-my-zsh"
+  fi
+  ZSH="$ZSH" command zsh -f "$ZSH/tools/upgrade.sh" || return $?
+
+  echo "updating zsh-autosuggestions"
+  git -C $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions pull >/dev/null
+
+  echo "updating zsh-syntax-highlighting"
+  git -C $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting pull >/dev/null
+
+}
+
 upgrade_node() {
   source $DOTFILE_DIR/dev/.environments.zsh
 
