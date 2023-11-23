ff03dc89b71f6b47689c9222e6b28a1236c7578e
Chongyi Zheng <git@zcy.dev>
2023-11-23 18:51:46 -0500

    Change

--- script/upgrade/upgrade_common.sh
+++ script/upgrade/upgrade_common.sh
@@ -29,7 +29,7 @@ upgrade_node() {
   PROFILE=/dev/null # avoid nvm to modify .zshrc
   nvm_latest=$(curl -q -w "%{url_effective}\\n" -L -s -S https://latest.nvm.sh -o /dev/null)
   nvm_latest=${nvm_latest##*/}
-  curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_latest}/install.sh" | zsh
+  curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_latest}/install.sh" | bash
 
   # upgrade node
   node_latest=$(nvm ls-remote | grep -i 'latest' | tail -n 1 | awk '{print $1}' | strings)
