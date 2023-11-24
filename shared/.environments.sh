d0e0bdf154285ce5f48041ddd6d77c8c4dd7f275
Chongyi Zheng <git@zcy.dev>
2023-11-23 20:48:17 -0500

    Fix lint

--- shared/.environments.sh
+++ shared/.environments.sh
@@ -5,7 +5,7 @@ if [[ -z "$IS_DEV_MACHINE" ]]; then
   if [[ "$os" == 'macos' ]]; then
     IS_DEV_MACHINE=true
   elif [[ "$os" == 'linux' ]]; then
-    if [[ $(hostname -s) = gpu-* ]]; then
+    if [[ "$(hostname -s)" = gpu-* ]]; then
       IS_DEV_MACHINE=true
     fi
   fi
