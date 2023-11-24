4239c566b4eb10a06660b7dae83f1f88ae3b05e9
Chongyi Zheng <git@zcy.dev>
2023-11-23 20:43:40 -0500

    Test dev machine script

--- shared/.environments.sh
+++ shared/.environments.sh
@@ -1,10 +1,12 @@
-IS_DEV_MACHINE=false
+if [[ -z "$IS_DEV_MACHINE" ]]; then
+  IS_DEV_MACHINE=false
 
-os=$(detect_os)
-if [[ "$os" == 'macos' ]]; then
-  IS_DEV_MACHINE=true
-elif [[ "$os" == 'linux' ]]; then
-  if [[ $(hostname -s) = gpu-* ]]; then
+  os=$(detect_os)
+  if [[ "$os" == 'macos' ]]; then
     IS_DEV_MACHINE=true
+  elif [[ "$os" == 'linux' ]]; then
+    if [[ $(hostname -s) = gpu-* ]]; then
+      IS_DEV_MACHINE=true
+    fi
   fi
 fi
