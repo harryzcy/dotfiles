5493359d727b3895d749f60656793bf6e7d7a98e
Chongyi Zheng <git@zcy.dev>
2023-12-29 00:46:59 +0800

    [dotd] Add structured logging

--- dotd/handler.go
+++ dotd/handler.go
@@ -1,55 +1,68 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 // ping returns a pong message
-func ping(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	_, _ = w.Write([]byte("pong\n"))
+func ping(logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("request received", zap.String("path", r.URL.Path))
+
+		w.Header().Set("Content-Type", "text/plain")
+		_, _ = w.Write([]byte("pong\n"))
+	}
 }
 
 // ejectDisks ejects all external disks
-func ejectDisks(w http.ResponseWriter, r *http.Request) {
-	running, err := isTMRunning()
-	if err != nil {
-		fmt.Println(err)
-		fail(w)
-		return
-	}
-	if running {
-		if err = stopTMBackup(); err != nil {
+func ejectDisks(logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("request received", zap.String("path", r.URL.Path))
+
+		running, err := isTMRunning()
+		if err != nil {
+			logger.Error("failed to check if TM is running", zap.Error(err))
 			fail(w)
 			return
 		}
-
-		for {
-			running, err = isTMRunning()
-			if err != nil {
+		if running {
+			if err = stopTMBackup(); err != nil {
+				logger.Error("failed to stop TM backup", zap.Error(err))
 				fail(w)
 				return
 			}
-			if !running {
-				break
+
+			for {
+				running, err = isTMRunning()
+				if err != nil {
+					logger.Error("failed to check if TM is running", zap.Error(err))
+					fail(w)
+					return
+				}
+				if !running {
+					break
+				}
 			}
 		}
-	}
-	mountPath, err := getTMMountPoint()
-	fmt.Println("found mount path: " + mountPath)
-	if err != nil {
-		fail(w)
-		return
-	}
-	if mountPath != "" {
-		if err := ejectDisk(mountPath); err != nil {
+		mountPath, err := getTMMountPoint()
+		logger.Info("found mount path", zap.String("mountPath", mountPath))
+		if err != nil {
+			logger.Error("failed to get TM mount point", zap.Error(err))
 			fail(w)
 			return
 		}
-	}
+		if mountPath != "" {
+			if err := ejectDisk(mountPath); err != nil {
+				logger.Error("failed to eject disk", zap.Error(err))
+				fail(w)
+				return
+			}
+		}
 
-	success(w)
+		success(w)
+	}
 }
 
 func success(w http.ResponseWriter) {
