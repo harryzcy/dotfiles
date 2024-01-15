78692849842d3ebe6b1a31232985fb04e038428a
Chongyi Zheng <git@zcy.dev>
2024-01-16 07:17:28 +0800

    Fix ncu lazy load (#162)

--- dev/.environments.zsh
+++ dev/.environments.zsh
@@ -34,7 +34,7 @@ conda() {
 # Node & nvm
 # nvm
 lazy_load_nvm() {
-  unset -f npm node nvm npx yarn sls
+  unset -f npm node nvm npx yarn sls ncu
   export NVM_DIR=~/.nvm
   [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
