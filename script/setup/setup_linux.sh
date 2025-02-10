da73a6f99f1c315dc2ee9a4d7bb069c5a01565ff
Chongyi Zheng <git@zcy.dev>
2025-02-10 04:14:50 -0500

    Remore old sourcing script

--- script/setup/setup_linux.sh
+++ script/setup/setup_linux.sh
@@ -28,7 +28,6 @@ install_tools:node() {
     brew install asdf
   fi
 
-  . "$HOME/.asdf/asdf.sh"
   asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
   asdf install nodejs latest
