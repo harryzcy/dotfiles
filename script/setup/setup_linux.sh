5d5c5b1150d52d86464129a784b3f1a25985b991
Chongyi Zheng <git@zcy.dev>
2025-03-27 00:18:51 -0400

    [linux] Install uv for python

--- script/setup/setup_linux.sh
+++ script/setup/setup_linux.sh
@@ -35,6 +35,20 @@ install_tools:node() {
   asdf set --home nodejs latest
 }
 
+install_tools:python() {
+  if ! command -v uv &>/dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+  fi
+
+  if ! command -v pipx &>/dev/null; then
+    echo "installing pipx"
+    python3 -m pip install --user pipx
+    python3 -m pipx ensurepath
+  fi
+  pipx install pip
+  pipx install httpie
+}
+
 install_tools:dev() {
   (
     set -x
@@ -50,14 +64,7 @@ install_tools:dev() {
   run_apt_install cloc
   run_apt_install jq
   install_tools:node
-
-  if ! command -v pipx &>/dev/null; then
-    echo "installing pipx"
-    python3 -m pip install --user pipx
-    python3 -m pipx ensurepath
-  fi
-  pipx install pip
-  pipx install httpie
+  install_tools:python
 }
 
 if [[ "${CODESPACES}" == 'true' ]]; then
