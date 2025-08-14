0e26a02f5b2d0ad7521ff7d72b5ac3707d66d0e9
Chongyi Zheng <git@zcy.dev>
2025-08-14 02:46:48 -0400

    [dev] Symlink bazel to bazelisk

--- script/setup/setup_linux.sh
+++ script/setup/setup_linux.sh
@@ -27,6 +27,7 @@ install_tools() {
   latest_version=$(curl -s https://api.github.com/repos/bazelbuild/bazelisk/releases/latest | jq -r .tag_name)
   curl -L "https://github.com/bazelbuild/bazelisk/releases/download/$latest_version/bazelisk-linux-amd64" -o "$DOTFILE_DIR/dot/bin/bazelisk"
   chmod +x "$DOTFILE_DIR/dot/bin/bazelisk"
+  ln -sf "$DOTFILE_DIR/dot/bin/bazelisk" "$DOTFILE_DIR/dot/bin/bazel"
 }
 
 install_tools:node() {
